40846c516be782073f0b2d5d59f21f2f993dd36a
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-06-28 18:24:01 +0530

    welcome section done

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Welcome from '@/components/Welcome/Welcome'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+        <Welcome/>
     </main>
   )
 }
