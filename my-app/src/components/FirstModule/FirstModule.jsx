87d0fd96298228bd97f3c1e190119542e51d11c4
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-07-02 02:41:50 +0530

    herodone

---
+++ my-app/src/components/FirstModule/FirstModule.jsx
@@ -0,0 +1,15 @@
+import styles from "./firstmodule.module.css"
+const FirstModule = () =>{
+    return(
+        <div className={styles.firstgrand}>
+            <div className={styles.firstparent}>
+                <div className={styles.heroheaderparent}><h1  className={styles.heroheader}>The <img className={styles.lol} src="https://tooth-fairy.ancorathemes.com/wp-content/uploads/elementor/thumbs/blog-image12-copyright-q1kyopwkl3fqyaxccb5nmmnt3mqx49w1njh27b2cw2.jpg"></img> <p className={styles.leading}>leading</p> Dentistry</h1></div>
+                <div className={styles.herbuttonset}>
+                    <div className={styles.herobut1}>About Us</div>
+                    <div className={styles.herobut2}>Get an Appointment</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FirstModule;
